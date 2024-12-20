4384f2414ed3b07edf603f316726ea4acd4f1205
Jonathan Avraham <avrahamjonathan@gmail.com>
2024-12-20 11:16:54 -0500

    add display name UI for Quickbooks

--- src/pages/customer-info-page/CustomerInformationPage.tsx
+++ src/pages/customer-info-page/CustomerInformationPage.tsx
@@ -16,9 +16,10 @@ export const CustomerInformationPage = () => {
         <p>Provide your company details to personalize and streamline your integration experience.</p>
       </header>
 
-      <h5 className={'mb-5'}>Wild Apricot Information</h5>
+      <form>
+        <h5 className={'mb-4'}>Wild Apricot Information</h5>
 
-      <div id={"wa-content"}>
+        <div className={"form-content"}>
           <div className="row ">
             <div className="col-md-6 mb-3">
               <h6>Company Info <i className={'bi bi-person-circle ms-2'}></i></h6>
@@ -41,51 +42,76 @@ export const CustomerInformationPage = () => {
               </div>
             </div>
           </div>
-        <div className="row ">
-          <div className="col-md-6 mb-3">
-            <h6>Contact Info <i className={'bi bi-send-fill ms-2'}></i></h6>
-            <p>Provide your company info</p>
-          </div>
-          <div className="col-md-6 mb-3">
-            <div className="mb-3">
-              <label htmlFor="email" className=" ">Email Address</label>
-              <input type="text" className="form-control" id="email"/>
+          <div className="row ">
+            <div className="col-md-6 mb-3">
+              <h6>Contact Info <i className={'bi bi-send-fill ms-2'}></i></h6>
+              <p>Provide your company info</p>
             </div>
-            <div className="mb-3">
-              <label htmlFor="phone-number" className=" ">Phone Number</label>
-              <input type="text" className="form-control" id="phone-number"/>
+            <div className="col-md-6 mb-3">
+              <div className="mb-3">
+                <label htmlFor="email" className=" ">Email Address</label>
+                <input type="text" className="form-control" id="email"/>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="phone-number" className=" ">Phone Number</label>
+                <input type="text" className="form-control" id="phone-number"/>
+              </div>
+            </div>
+          </div>
+          <div className="row ">
+            <div className="col-md-6 mb-3">
+              <h6>Location Info <i className={'bi bi-geo-alt-fill ms-2'}></i></h6>
+              <p>Provide your company info</p>
+            </div>
+            <div className="col-md-6 mb-3">
+              <div className="row">
+                <div className="col">
+                  <label htmlFor="address" className=" ">Street Address</label>
+                  <input type="text" className="form-control" id="address"/>
+                </div>
+                <div className="col">
+                  <label htmlFor="country" className=" ">Country</label>
+                  <input type="text" className="form-control" id="country"/>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <label htmlFor="state" className=" ">State</label>
+                  <input type="text" className="form-control" id="state"/>
+                </div>
+                <div className="col">
+                  <label htmlFor="city" className=" ">City</label>
+                  <input type="text" className="form-control" id="city"/>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="row ">
-          <div className="col-md-6 mb-3">
-            <h6>Location Info <i className={'bi bi-geo-alt-fill ms-2'}></i></h6>
-            <p>Provide your company info</p>
-          </div>
-          <div className="col-md-6 mb-3">
-            <div className="row">
+
+        <h5 className={"mt-4 mb-4"}>Quickbooks Information</h5>
+        <div className="form-content">
+          <div className="row ">
+            <div className="col-md-6 mb-3">
+              <h6>Display Name</h6>
+              <p>Construct name using your Wild Apricot fields below</p>
+            </div>
+            <div className="col-md-6 mb-3">
               <div className="col">
-                <label htmlFor="address" className=" ">Street Address</label>
-                <input type="text" className="form-control" id="address"/>
-              </div>
-              <div className="col">
-                <label htmlFor="country" className=" ">Country</label>
-                <input type="text" className="form-control" id="country"/>
+                <input type="text" className="form-control" id="display-name" placeholder={"Display Name"}/>
               </div>
             </div>
-            <div className="row">
-              <div className="col">
-                <label htmlFor="state" className=" ">State</label>
-                <input type="text" className="form-control" id="state"/>
-              </div>
-              <div className="col">
-                <label htmlFor="city" className=" ">City</label>
-                <input type="text" className="form-control" id="city"/>
-              </div>
+            <div className="col-md-6">
+              {/*<button className={'col'}>First Name</button>*/}
+              {/*<button className={'col'}>First Name</button>*/}
+              {/*<button className={'col'}>First Name</button>*/}
+              {/*<button className={'col'}>First Name</button>*/}
+              {/*<button className={'col'}>First Name</button>*/}
+
             </div>
           </div>
         </div>
-      </div>
+        <button className={"mt-4"} type={"submit"}>Next</button>
+      </form>
 
     </main>
   )
