19a7f3d6d146362d8fd3fd09ed5002ce796b4bde
Jonathan Avraham <avrahamjonathan@gmail.com>
2025-02-12 15:01:21 -0500

    finish scheduling page

--- src/components/scheduling-component/SchedulingComponent.tsx
+++ src/components/scheduling-component/SchedulingComponent.tsx
@@ -1,16 +1,33 @@
 import * as React from "react";
 import './Scheduling.css'
 import {useState} from "react";
+import {Action, SchedulingData} from "../../pages/scheduling-page/SchedulingPage.tsx";
 interface SchedulingComponentProps {
-  title: string
+  title: string,
+  schedulingData: SchedulingData,
+  dispatch: React.Dispatch<Action>
 }
 
-export const SchedulingComponent = ({ title }: SchedulingComponentProps) => {
+export const SchedulingComponent = ({ title, schedulingData, dispatch }: SchedulingComponentProps) => {
 
-  const [jobType, setJobType] = useState("")
+
+  const handleJobTypeChange = (e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+    const { value } = e.target;
+    dispatch({ type: "SET_JOB_TYPE", payload: value });
+  };
+
+  const handleManualChange = (e) => {
+    const { name, value } = e.target
+    dispatch({ type: "UPDATE_MANUAL_SCHEDULE", payload: { [name]: value } });
+  }
+
+  const handleScheduledChange = (e) => {
+    const { name, value } = e.target
+    dispatch({ type: "UPDATE_SCHEDULED_SCHEDULE", payload: { [name]: value } });
+  }
 
   const renderForm = () => {
-    if(jobType == 'Manual'){
+    if(schedulingData.jobType == 'Manual'){
       return (
         <>
           <div className="row g-3">
@@ -18,20 +35,20 @@ export const SchedulingComponent = ({ title }: SchedulingComponentProps) => {
               <label htmlFor={`${title.toLowerCase()}-manual-start-input`} className="form-label">Manual Start Time</label>
               <div className="input-group input-group-sm">
                 <span className="input-group-text" id="basic-addon1"><i className={'bi bi-calendar'}></i></span>
-                <input id={`${title.toLowerCase()}-manual-start-input`} type="date" className="form-control" placeholder="Choose Date" aria-label="Username" aria-describedby="basic-addon1" />
+                <input onChange={handleManualChange} value={schedulingData.manualSchedule.startDate} name={'startDate'} id={`${title.toLowerCase()}-manual-start-input`} type="date" className="form-control" placeholder="Choose Date" aria-label="Username" aria-describedby="basic-addon1" />
               </div>
             </div>
             <div className="col-md-6">
               <label htmlFor={`${title.toLowerCase()}-manual-end-input`} className="form-label">Manual End Time</label>
               <div className="input-group input-group-sm">
                 <span className="input-group-text" id="basic-addon1"><i className={'bi bi-calendar'}></i></span>
-                <input id={`${title.toLowerCase()}-manual-end-input`} type="date" className="form-control" placeholder="Choose Date" aria-label="Username" aria-describedby="basic-addon1" />
+                <input onChange={handleManualChange} value={schedulingData.manualSchedule.endDate} name={'endDate'} id={`${title.toLowerCase()}-manual-end-input`} type="date" className="form-control" placeholder="Choose Date" aria-label="Username" aria-describedby="basic-addon1" />
               </div>
             </div>
             <div className="col-md-6">
               <label htmlFor={`${title.toLowerCase()}-doc-input`} className="form-label">Manual {title} {title.toLowerCase() === 'payment' ? 'ID' : 'Doc'} #</label>
               <div className="input-group input-group-sm">
-                <input id={`${title.toLowerCase()}-doc-input`} type="text" className="form-control" placeholder="Enter here..." aria-label="Username" aria-describedby="basic-addon1" />
+                <input onChange={handleManualChange} value={schedulingData.manualSchedule.docNumber} name={'docNumber'} id={`${title.toLowerCase()}-doc-input`} type="text" className="form-control" placeholder="Enter here..." aria-label="Username" aria-describedby="basic-addon1" />
               </div>
             </div>
           </div>
@@ -40,33 +57,37 @@ export const SchedulingComponent = ({ title }: SchedulingComponentProps) => {
 
     }
 
-    else if(jobType == 'Scheduled'){
+    else if(schedulingData.jobType == 'Scheduled'){
       return (
         <>
           <div className="row g-3 align-items-center">
             <div className="col-md-6">
               <label htmlFor={`${title.toLowerCase()}-time-periods-input`} className="form-label">Time Period for each Run</label>
-              <select id={`${title.toLowerCase()}-time-periods-input`} className={'form-select'} defaultValue={""}>
+              <select onChange={handleScheduledChange} id={`${title.toLowerCase()}-time-periods-input`} name={'timePeriod'} className={'form-select'} value={schedulingData.scheduledSchedule.timePeriod}>
                 <option value={""}>Choose time period ...</option>
                 <option value={"Daily"}>Daily</option>
                 <option value={"Weekly"}>Weekly</option>
                 <option value={"Monthly"}>Monthly</option>
               </select>
             </div>
-            <div className="col-md-6">
-              <label htmlFor={`${title.toLowerCase()}-num-periods-input`} className="form-label"># of Time Periods</label>
-              <input id={`${title.toLowerCase()}-num-periods-input`} type="number" className="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" />
+            <div className="col-md-6 input-group-sm">
+              <label htmlFor={`${title.toLowerCase()}-num-periods-input `} className="form-label"># of Time Periods</label>
+              <input onChange={handleScheduledChange} value={schedulingData.scheduledSchedule.numOfTimePeriods} name={'numOfTimePeriods'} id={`${title.toLowerCase()}-num-periods-input`} type="number" className="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" />
             </div>
             <div className="col-md-6">
               <label htmlFor={`${title.toLowerCase()}-weekly-input`} className="form-label">Weekly: Day of week to start run</label>
-              <select id={`${title.toLowerCase()}-weekly-input`} className={'form-select'} defaultValue={""}>
+              <select onChange={handleScheduledChange} value={schedulingData.scheduledSchedule.dayOfWeek} name={'dayOfWeek'} id={`${title.toLowerCase()}-weekly-input`} className={'form-select'}>
                 <option value={""}>Choose day of the week ...</option>
                 {["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"].map((day, index) => <option key={index} value={day}>{day}</option>)}
               </select>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 input-group-sm">
               <label htmlFor={`${title.toLowerCase()}-monthly-input`} className="form-label">Monthly: Day of month to start run</label>
-              <input id={`${title.toLowerCase()}-monthly-input`} type="number" className="form-control" max={31} placeholder="" aria-label="Username" aria-describedby="basic-addon1" />
+              <input onChange={handleScheduledChange} value={schedulingData.scheduledSchedule.dayOfMonth} name={'dayOfMonth'} id={`${title.toLowerCase()}-monthly-input`} type="number" className="form-control" max={31} placeholder="" aria-label="Username" aria-describedby="basic-addon1" />
+            </div>
+            <div className="col-md-4 input-group-sm">
+              <label htmlFor={`${title.toLowerCase()}-start-time-input`} className="form-label">Start time for run</label>
+              <input onChange={handleScheduledChange} value={schedulingData.scheduledSchedule.startTime} name={'startTime'} id={`${title.toLowerCase()}-start-time-input`} type="time" className="form-control" max={31} placeholder="" aria-label="Username" aria-describedby="basic-addon1" />
             </div>
           </div>
         </>
@@ -79,7 +100,7 @@ export const SchedulingComponent = ({ title }: SchedulingComponentProps) => {
       <h6>{title} Scheduling</h6>
       <div className="col-md-5">
         <label htmlFor={`${title.toLowerCase()}-job-type-input`} className="form-label">Job Scheduling Type</label>
-        <select id={`${title.toLowerCase()}-job-type-input`} className={'form-select'} defaultValue={""} onChange={(e) => setJobType(e.target.value)} value={jobType}>
+        <select id={`${title.toLowerCase()}-job-type-input`} name={'jobType'} className={'form-select'} onChange={(e) => dispatch({ type: "SET_JOB_TYPE", payload: e.target.value })} value={schedulingData.jobType}>
           <option value={""}>Choose job type ...</option>
           <option value={"Manual"}>Manual</option>
           <option value={"Scheduled"}>Scheduled</option>
--- src/pages/scheduling-page/SchedulingPage.tsx
+++ src/pages/scheduling-page/SchedulingPage.tsx
@@ -1,21 +1,82 @@
 import {useOnBoarding} from "../../hooks/useOnboarding.ts";
-import {useEffect, useState} from "react";
+import {useEffect, useReducer, useState} from "react";
 import * as React from "react";
 import {useNavigate} from "react-router-dom";
 import {PageTemplate} from "../../components/page-template/PageTemplate.tsx";
 import './Scheduling.css'
 import {SchedulingComponent} from "../../components/scheduling-component/SchedulingComponent.tsx";
+import {toast} from "react-toastify";
+import update = toast.update;
+
+export interface ManualSchedule {
+  startDate: string,
+  endDate: string,
+  docNumber: string
+}
+
+export interface ScheduledSchedule {
+  timePeriod: string,
+  numOfTimePeriods: number,
+  dayOfWeek: string,
+  dayOfMonth: number,
+  startTime: string
+}
+
+export interface SchedulingData {
+  jobType: string,
+  manualSchedule: ManualSchedule,
+  scheduledSchedule: ScheduledSchedule
+}
+
+export type Action =
+  | { type: "SET_JOB_TYPE"; payload: string }
+  | { type: "UPDATE_MANUAL_SCHEDULE"; payload: any }
+  | { type: "UPDATE_SCHEDULED_SCHEDULE"; payload: any };
+
+const initialState: SchedulingData = {
+  jobType: "",
+  manualSchedule: { startDate: "", endDate: "", docNumber: "" },
+  scheduledSchedule: { timePeriod: "", numOfTimePeriods: 0, dayOfMonth: 1, dayOfWeek: "", startTime: "" },
+};
+
+const schedulingReducer = (state: SchedulingData, action: Action): SchedulingData => {
+  switch (action.type) {
+    case "SET_JOB_TYPE":
+      return { ...state, jobType: action.payload };
+
+    case "UPDATE_MANUAL_SCHEDULE":
+      return { ...state, manualSchedule: { ...state.manualSchedule, ...action.payload } };
+
+    case "UPDATE_SCHEDULED_SCHEDULE":
+      return { ...state, scheduledSchedule: { ...state.scheduledSchedule, ...action.payload } };
+
+    default:
+      return state;
+  }
+};
+
+const init = (state) => {
+  return state
+}
 
 export const SchedulingPage = () => {
-  const { setCurrentStep } = useOnBoarding()
+  const { setCurrentStep, updateData, onBoardingData } = useOnBoarding()
   const navigate = useNavigate()
 
   const [errorMsg, setErrorMsg] = useState('');
 
+  const [invoiceScheduling, dispatchInvoice] = useReducer(schedulingReducer, onBoardingData.invoiceScheduling ??  initialState, init);
+  const [paymentScheduling, dispatchPayment] = useReducer(schedulingReducer, onBoardingData.paymentScheduling ?? initialState, init);
+  const [donationScheduling, dispatchDonation] = useReducer(schedulingReducer, onBoardingData.donationScheduling ?? initialState, init);
+
   useEffect(() => {
     setCurrentStep(7)
-
+    console.log(onBoardingData)
   }, []);
+
+  useEffect(() => {
+    updateData({ invoiceScheduling, paymentScheduling, donationScheduling})
+  }, [invoiceScheduling, paymentScheduling, donationScheduling]);
 
   const handleSubmission = () => {
     navigate('/clone-scenarios')
@@ -29,9 +90,9 @@ export const SchedulingPage = () => {
       validate={handleSubmission}
       errorMsg={errorMsg}
     >
-      <SchedulingComponent title={'Invoice'}/>
-      <SchedulingComponent title={'Payment'}/>
-      <SchedulingComponent title={'Donation'}/>
+      <SchedulingComponent title={'Invoice'} schedulingData={invoiceScheduling} dispatch={dispatchInvoice}/>
+      <SchedulingComponent title={'Payment'} schedulingData={paymentScheduling} dispatch={dispatchPayment}/>
+      <SchedulingComponent title={'Donation'} schedulingData={donationScheduling} dispatch={dispatchDonation}/>
 
     </PageTemplate>
   )
