c5b91d848ff25e3f1f88f39d21a9332bb261481a
Jonathan Avraham <avrahamjonathan@gmail.com>
2025-02-16 10:56:44 -0500

    add auto scrolling for sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,10 +2,21 @@ import "./Sidebar.css";
 import {steps} from "../../onboardingSteps.ts";
 import {useOnBoarding} from "../../hooks/useOnboarding.ts";
 import {Link, useNavigate} from "react-router-dom";
+import {useEffect, useRef} from "react";
 
 const Sidebar: React.FC = () => {
   const { currentStep } = useOnBoarding();
   const navigate = useNavigate()
+  const stepRefs = useRef<(HTMLLIElement | null)[]>(steps.map(step => null)); // Array of refs for steps
+
+  useEffect(() => {
+    if ("scrollIntoView" in stepRefs.current[currentStep - 1]) {
+      stepRefs.current[currentStep - 1]?.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  }, [currentStep]); // Run when currentStep changes
 
   return (
     <aside className="sidebar">
@@ -16,23 +27,7 @@ const Sidebar: React.FC = () => {
           steps.map((step, index) => {
             const isClickable = index + 1 < currentStep
             return (
-              <li className={isClickable ? "completed-step" : index + 1 == currentStep  ? "active-step" : ""} key={index}>
-                {/*{isClickable ?*/}
-                {/*  <Link className="step-number" to={step.endpoint}>*/}
-                {/*    {index + 1 < currentStep ? <i style={{fontSize: "1.2em", color: "white"}} className={"bi bi-check"}></i> : index + 1}*/}
-                {/*    <div className={`progressing`} style={{ height: `${index + 1 < currentStep ? '45px' : '0px'}` }}/>*/}
-                {/*  </Link> :*/}
-                {/*  <span className="step-number">*/}
-                {/*    {index + 1 < currentStep ? <i style={{fontSize: "1.2em", color: "white"}} className={"bi bi-check"}></i> : index + 1}*/}
-                {/*    <div*/}
-                {/*      className="progressing"*/}
-                {/*      style={{*/}
-                {/*        height: `${index + 1 < currentStep ? '45px' : '0px'}`,*/}
-                {/*      }}*/}
-                {/*    />*/}
-                {/*  </span>*/}
-                {/*}*/}
-
+              <li ref={(el) => { stepRefs.current[index] = el; }} className={isClickable ? "completed-step" : index + 1 == currentStep  ? "active-step" : ""} key={index}>
                 <div
                   className={`step-number ${isClickable ? "clickable" : ""}`}
                   onClick={() => step.endpoint && navigate(step.endpoint)} // Use navigate() for routing
